9748e3554865f706c52b4fc01dadce77a066b2bf
Zhicheng Liu <liupostno@gmail.com>
2024-11-21 03:22:26 -0500

    Fixed ESLint warnings in Dashboard and Modules components

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -115,11 +115,6 @@ export default function Dashboard({
     }
   };
 
-
-  useEffect(() => {
-    fetchData();
-  }, [currentUser?._id]);
-
   const handleUpdateCourse = async () => {
     if (!currentUser?._id) return;
 
